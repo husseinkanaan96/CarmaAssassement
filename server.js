05523863fe1b99ce6aa8a9c0199aa5d0c1ee9548
Hussein Kanaan <husseinkanaan1996@gmail.com>
2024-02-28 21:02:32 +0200

    commit

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const path = require('path');
-const creditCardRoutes = require('./routes');
+const knex = require('knex');
+const knexConfig = require('./knexfile');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api/creditCard', creditCardRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/api/submitData', async (req, res) => {
+
+    // Setup db connection
+    const db = knex(knexConfig);
+    const saltRounds = 5;
+    const data = {
+        name: req.body.cardHolderName,
+        card_number: await bcrypt.hash(req.body.cardNumber, saltRounds),
+        cvv: await bcrypt.hash(req.body.cvv, saltRounds),
+        expiry_date: req.body.expirationDate
+    };
+
+    await db('creditcard_info')
+        .insert(data)
+        .then(() => {
+            res.redirect('/?status=success');
+        }).catch((err) => {
+            res.redirect('/?status=error');
+        }).finally(() => {
+            db.destroy();
+        });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
